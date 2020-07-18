c8670d6e0ff98fd65a2c4d766277bc023cc498c1
Anthony Bratcher <tonybrat74@gmail.com>
2020-07-18 15:34:58 -0700

    startvalidation

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as yup from "yup";
 
-function Form () {
+
+const formSchema = yup.object().shape({
+    name: yup.string().required("Name is a required"),                              
+    toppings: yup.boolean().oneOf([true], "please choose a topping"),
+    special_instructions: yup.string().required("enter none if no special instructions")
+})
+
+
+function Form (props) {
 
     const [orderForm, setOrderForm] = useState({
     name:"",
@@ -8,56 +17,105 @@ function Form () {
     toppings:"",
     special_instructions:""
     });
-    
-    const placeOrder = event =>{
-    event.preventDefault();
-    console.log("order placed");
+
+    const[errrors, setErrrors] = useState({
+    name:"",
+    toppings:"",
+    special_instructions:""
+    })
+
+    const [buttonDisabled, setButtonDisabled]= useState(true);
+
+    useEffect(() => {
+        formSchema.isValid(orderForm).then(valid => {
+            setButtonDisabled(!valid)
+        })
+
+    },[orderForm])
+
+
+
+
+    const handlechanges = event =>{
+    setOrderForm({...orderForm, [event.target.name]: event.target.value});
+    console.log(orderForm);
     };
-    const makeOrder = event => {
-    console.log("added Input", event.target.value);
-    setOrderForm({ name: event.target.value});
+    const submitOrder = event => {
+        event.preventDefault();
+        props.placeNewOrder(orderForm)
+        setOrderForm({name:"", pizza_size:"", toppings:"", special_instructions:""})
+        
+    
     };
     
     
     return (
     <div className="placed-order">
-    <form onSubmit={placeOrder}>
+    <form onSubmit={submitOrder}>
     
-   <p> <label htmlFor="name">
+    <label htmlFor="name">
     Name
     <input 
     type="text" 
     id="name" 
     name="name"
-    onChange={ makeOrder }
+    onChange={ handlechanges }
     value={orderForm.name}
     />
     </label>
-    </p>
-    <p><label htmlFor="pizzaSize">
-    Pizza Size
-    <input 
-    type="dropdown" id="pizzaSize"
-    />
+
+    
+    
+   
+    <label htmlFor="pizza_size">
+        Choose your pizza size
+    <select id="pizza_size" name="pizza_size"
+    onChange={ handlechanges }>
+    <option value="small">Small</option>   
+    <option value="medium">Medium</option>
+    <option value="large">Large</option>
+    <option value="xlarge">XLarge</option>
+    </select>
     </label>
-    </p>
-    <label htmlFor="toppings">
+
     
+    Choose you toppings
+    <label htmlFor="ham">
+        Ham
+        <input type= "checkbox" 
+        name="ham" checked={orderForm.ham}
+        onChange={ handlechanges }/>
     </label>
-      
-      
+    <label htmlFor="pineapple">
+        Pineapple
+        <input type= "checkbox" 
+        name="pineapple" checked={orderForm.pineapple}  
+        onChange={ handlechanges }/>
+    </label>
+    <label htmlFor="sausage">
+        Sausage
+        <input type= "checkbox" 
+        name="sausage" checked={orderForm.pineapple}
+        onChange={ handlechanges }/>
+    </label>
+    <label htmlFor="pepperoni">
+        Pepperoni
+        <input type= "checkbox" 
+        name="pepperoni" checked={orderForm.pepperoni}
+        onChange={ handlechanges }/>
+    </label>
     
-    
-     <p><label htmlFor="instructions">
+     <label htmlFor="special_instructions">
          Special Instructions
-        <textarea id="instructions"
-         name="instructions" />
-        
+        <textarea id="special_instructions"
+         name="special_instructions" 
+          onChange={ handlechanges }
+          value= {orderForm.special_instructions}  />
         </label> 
-        </p>
+        
     
     
-    <button type="submit">Add to Order</button>
+    <button disabled={buttonDisabled}>Add to Order</button>
     </form>
     </div>
     );
