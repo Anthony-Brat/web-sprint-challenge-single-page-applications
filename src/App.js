dd1094b064cb5f889a86fc9626144708b420419a
Anthony Bratcher <tonybrat74@gmail.com>
2020-07-19 10:33:31 -0700

    postrequest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from 'axios';
 import { Route, Switch, Link } from 'react-router-dom';
 import Form from './Form';
 
--- src/Form.js
+++ src/Form.js
@@ -1,177 +1,208 @@
 import React, { useState, useEffect } from 'react';
 import * as yup from "yup";
+import axios from 'axios';
 
 
 const formSchema = yup.object().shape({
-    name: yup
-    .string()
-    .min(2,"name must be at least 2 characters long")
-    .required("Name is a required"),                              
-    ham: yup
-    .boolean()
-    .oneOf([true], "please choose a topping"),
-    pineapple: yup
-    .boolean()
-    .oneOf([true], "please choose a topping"),
-    sausage: yup
-    .boolean()
-    .oneOf([true], "please choose a topping"),
-    pepperoni: yup
-    .boolean()
-    .oneOf([true], "please choose a topping"),
-    special_instructions: yup
-    .string()
-    .required("enter none if no special instructions")
+name: yup
+.string()
+.min(2,"name must be at least 2 characters long")
+.required("Name is a required"),
+pizza_size: yup
+.string(),                              
+ham: yup
+.boolean()
+.oneOf([true], "please choose a topping"),
+pineapple: yup
+.boolean()
+.oneOf([true], "please choose a topping"),
+sausage: yup
+.boolean()
+.oneOf([true], "please choose a topping"),
+pepperoni: yup
+.boolean()
+.oneOf([true], "please choose a topping"),
+special_instructions: yup
+.string()
+.required("enter none if no special instructions")
 })
 
 
 function Form (props) {
 
-    const [orderForm, setOrderForm] = useState({
-    name:"",
-    pizza_size:"",
-    toppings:"",
-    special_instructions:""
-    });
+const [orderForm, setOrderForm] = useState({
+name:"",
+pizza_size:"",
+toppings:"",
+// ham:"",
+// pineapple:"",
+// sausage: "",
+// pepperoni: "",
+special_instructions:""
+});
 
-    const[errors, setErrors] = useState({
-    name:"",
-    toppings:"",
-    special_instructions:""
+const[errors, setErrors] = useState({
+name:"",
+toppings:"",
+// ham:"",
+// pineapple:"",
+// sausage: "",
+// pepperoni: "",
+special_instructions:""
+})
+
+const [buttonDisabled, setButtonDisabled]= useState(true);
+
+
+const [post, setPost] = useState([]);
+
+useEffect(() => {
+    formSchema.isValid(orderForm).then(valid => {
+        setButtonDisabled(!valid)
     })
 
-    const [buttonDisabled, setButtonDisabled]= useState(true);
+},[orderForm])
 
-    useEffect(() => {
-        formSchema.isValid(orderForm).then(valid => {
-            setButtonDisabled(!valid)
+const validateChange = event =>{
+    yup.reach(formSchema, event.target.name)
+    .validate(event.target.value)
+    .then(valid =>{
+        setErrors({
+            ...errors, [event.target.name] : ""
+        });
+    })
+    .catch(error =>{
+        setErrors({
+            ...errors, [event.target.name] : error.errors[0]
         })
-
-    },[orderForm])
-
-    const validateChange = event =>{
-        yup.reach(formSchema, event.target.name)
-        .validate(event.target.value)
-        .then(valid =>{
-            setErrors({
-                ...errors, [event.target.name] : ""
-            });
-        })
-        .catch(error =>{
-            setErrors({
-                ...errors, [event.target.name] : error.errors[0]
-            })
-        })
-    }
+    })
+}
 
 
-    const handleChanges = event =>{
-    //     console.log(orderForm);
-    // setOrderForm({...orderForm, [event.target.name]: event.target.value});
-    event.persist();
-    const newOrderForm = {
-        ...orderForm, [event.target.name] : event.target.type ===
-        "checkbox" ? event.target.checked : event.target.value
-    }
-    validateChange(event);
-    setOrderForm(newOrderForm);
+const handleChanges = event =>{
+//     console.log(orderForm);
+// setOrderForm({...orderForm, [event.target.name]: event.target.value});
+event.persist();
+const newOrderForm = {
+    ...orderForm, [event.target.name] : event.target.type ===
+    "checkbox" ? event.target.checked : event.target.value
+}
+validateChange(event);
+setOrderForm(newOrderForm);
 };
-    const submitOrder = event => {
-        event.preventDefault();
-        console.log("order placed")
-        
-        
-    
-    };
-    
-    
-    return (
-    <div className="placed-order">
-    <form onSubmit={submitOrder}>
-    
-    <label htmlFor="name">
-    Name
-    <input 
-    type="text" 
-    id="name" 
-    name="name"
-    onChange={ handleChanges }
-    value={orderForm.name}
-    />
-    {errors.name.length > 2 ? <p className="error">
-    {errors.name}</p> : null}
-    </label>
+const submitOrder = event => {
+    event.preventDefault();
+    // console.log("order placed")
+    axios.post("https://reqres.in/api/users", orderForm)
+    .then(response => {
+            setPost(response.data)
+            console.log("success", post);
 
-    <label htmlFor="pizza_size">
-    Choose your pizza size
-    <select 
-    id="pizza_size" 
-    name="pizza_size"
-    onChange={ handleChanges }>
-    <option value="small">Small</option>   
-    <option value="medium">Medium</option>
-    <option value="large">Large</option>
-    <option value="xlarge">XLarge</option>
-    </select>
-    </label>
+            setOrderForm({
+                name:"",
+                // ham:"",
+                // pineapple:"",
+                // sausage: "",
+                // pepperoni: "",
+                special_instructions:""
+            })
+    })
+            .catch(error =>{
+                console.log(error.response)
+            })
+    
 
-    
-    Choose you toppings
-    <label htmlFor="ham" className= "ham">
-    Ham
-    <input 
-    type= "checkbox" 
-    name="ham" 
-    checked={orderForm.ham}
-    onChange={ handleChanges }/>
-    </label>
+};
 
-    <label htmlFor="pineapple"className= "pineapple">
-    Pineapple
-    <input 
-    type= "checkbox" 
-    name="pineapple" 
-    checked={orderForm.pineapple}  
-    onChange={ handleChanges }/>
-    </label>
 
-    <label htmlFor="sausage" className= "sausage">
-    Sausage
-    <input 
-    type= "checkbox" 
-    name="sausage" 
-    checked={orderForm.sausage}
-    onChange={ handleChanges }/>
-    </label>
+return (
+<div className="placed-order">
+<form onSubmit={submitOrder}>
 
-    <label htmlFor="pepperoni" className= "pepperoni">
-    Pepperoni
-    <input type= "checkbox" 
-    name="pepperoni" 
-    checked={orderForm.pepperoni}
-    onChange={ handleChanges }/>
-    </label>
-    
-    <label htmlFor="special_instructions">
-    Special Instructions
-    <textarea 
-    id="special_instructions"
-    name="special_instructions" 
-    onChange={ handleChanges }
-    value= {orderForm.special_instructions}/>
-    {errors.special_instructions.length > 2 ? (<p className="error">
-    {errors.special_instructions}</p>) :null}
-    </label> 
-        
-    
-    
-    <button disabled={buttonDisabled}>Add to Order</button>
-    </form>
-    </div>
-    );
-    }
-    
-    export default Form;
+<label htmlFor="name">
+Name
+<input 
+type="text" 
+id="name" 
+name="name"
+onChange={ handleChanges }
+value={orderForm.name}
+/>
+{errors.name.length > 2 ? <p className="error">
+{errors.name}</p> : null}
+</label>
+
+<label htmlFor="pizza_size">
+Choose your pizza size
+<select 
+id="pizza_size" 
+name="pizza_size"
+onChange={ handleChanges }>
+<option value="small">Small</option>   
+<option value="medium">Medium</option>
+<option value="large">Large</option>
+<option value="xlarge">XLarge</option>
+</select>
+</label>
+
+
+Choose you toppings
+<label htmlFor="ham" className= "ham">
+Ham
+<input 
+type= "checkbox" 
+name="ham" 
+checked={orderForm.ham}
+onChange={ handleChanges }/>
+</label>
+
+<label htmlFor="pineapple"className= "pineapple">
+Pineapple
+<input 
+type= "checkbox" 
+name="pineapple" 
+checked={orderForm.pineapple}  
+onChange={ handleChanges }/>
+</label>
+
+<label htmlFor="sausage" className= "sausage">
+Sausage
+<input 
+type= "checkbox" 
+name="sausage" 
+checked={orderForm.sausage}
+onChange={ handleChanges }/>
+</label>
+
+<label htmlFor="pepperoni" className= "pepperoni">
+Pepperoni
+<input type= "checkbox" 
+name="pepperoni" 
+checked={orderForm.pepperoni}
+onChange={ handleChanges }/>
+</label>
+
+<label htmlFor="special_instructions">
+Special Instructions
+<textarea 
+id="special_instructions"
+name="special_instructions" 
+onChange={ handleChanges }
+value= {orderForm.special_instructions}/>
+{errors.special_instructions.length > 0 ? (<p className="error">
+{errors.special_instructions}</p>) :null}
+</label> 
+
+<pre>{JSON.stringify(post, null, 2)}</pre>  
+
+
+<button disabled={buttonDisabled}>Add to Order</button>
+</form>
+</div>
+);
+}
+
+export default Form;
 
 
 
